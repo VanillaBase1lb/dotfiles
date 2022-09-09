8b790ca0e2113367e08d2c478bd49f191f0b5634
Yuvraj Mishra <40194399+VanillaBase1lb@users.noreply.github.com>
2022-09-09 16:33:32 +0530

    bind copilot to <c-j>

--- init.lua
+++ init.lua
@@ -1005,7 +1005,8 @@ require("gitsigns").setup({
 
 -- Copilot
 vim.g.copilot_node_command = "~/.nvm/versions/node/v17.9.1/bin/node"
--- vim.g.copilot_no_tab_map = true
--- vim.g.copilot_assume_mapped = true
--- vim.g.copilot_tab_fallback = ""
--- vim.keymap.set("i", "<C-J>", vim.fn["copilot#Accept"], opts)
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_assume_mapped = true
+vim.g.copilot_tab_fallback = ""
+-- vim.keymap.set("i", "<C-J>", vim.fn["copilot#Accept()"], opts)
+vim.api.nvim_set_keymap('i', '<c-j>', 'copilot#Accept()', { expr = true })
