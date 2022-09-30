416b6d537b0b674aa1788474f7c5d573abd9e301
Yuvraj Mishra <40194399+VanillaBase1lb@users.noreply.github.com>
2022-09-30 19:08:34 +0530

    use swapfile

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@
 vim.opt.cmdheight = 1 -- more space in the neovim command line for displaying messages
 vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 vim.opt.conceallevel = 0 -- so that `` is visible in markdown files
-vim.opt.fileencoding = "utf-32" -- the encoding written to a file
+vim.opt.fileencoding = "utf-8" -- the encoding written to a file
 vim.opt.hlsearch = true -- highlight all matches on previous search pattern
 vim.opt.ignorecase = true -- ignore case in search patterns
 vim.opt.mouse = "a" -- allow the mouse to be used in neovim
@@ -14,7 +14,7 @@ vim.opt.smartcase = true -- smart case
 vim.opt.smartindent = true -- make indenting smarter again
 vim.opt.splitbelow = true -- force all horizontal splits to go below current window
 vim.opt.splitright = true -- force all vertical splits to go to the right of current window
-vim.opt.swapfile = false -- creates a swapfile
+-- vim.opt.swapfile = false -- creates a swapfile
 vim.opt.termguicolors = true -- set term gui colors (most terminals support this)
 vim.opt.timeoutlen = 1000 -- time to wait for a mapped sequence to complete (in milliseconds)
 vim.opt.undofile = true -- enable persistent undo
@@ -82,7 +82,7 @@ vim.keymap.set({ "n", "v", "x" }, "<S-h>", ":bprevious<CR>", opts)
 vim.keymap.set({ "n", "v", "x" }, "Q", ":bp|bd #<CR>", opts)
 vim.keymap.set({ "n", "v", "x" }, "<leader>bd", ":bd!<CR>", opts)
 vim.keymap.set({ "n", "v", "x" }, "<leader>bD", ":bp|bd! #<CR>", opts)
-vim.keymap.set({ "n", "v", "x" }, "<leader>w", ":update<CR>", opts)
+vim.keymap.set({ "n", "v", "x" }, "<leader>w", ":w<CR>", opts)
 vim.keymap.set({ "n", "v", "x" }, "<leader>q", ":q<CR>", opts)
 vim.keymap.set({ "n", "v", "x" }, "<leader>bt", ":vnew<CR>", opts) -- create new temporary buffer in vsplit
 vim.keymap.set({ "n", "v", "x" }, "<leader>bs", ":BufferLinePick<CR>", opts) -- create new temporary buffer in vsplit
@@ -451,6 +451,7 @@ null_ls.setup({
 		-- null_ls.builtins.formatting.fixjson,
 		-- null_ls.builtins.formatting.eslint,
 		null_ls.builtins.formatting.shfmt,
+		null_ls.builtins.formatting.markdownlint,
 		-- null_ls.builtins.formatting.goimports,
 		-- null_ls.builtins.code_actions.eslint,
 		null_ls.builtins.code_actions.shellcheck,
@@ -459,6 +460,7 @@ null_ls.setup({
 			prefer_local = ".venv/bin",
 		}),
 		null_ls.builtins.diagnostics.golangci_lint,
+		null_ls.builtins.diagnostics.markdownlint,
 		-- null_ls.builtins.diagnostics.luacheck,
 		null_ls.builtins.completion.spell.with({
 			filetypes = { "text", "markdown" }
