d00eabfd1222e889705b3684b3e48126a025f6c8
Yuvraj Mishra <yuvraj.mishra@dhan.ai>
2023-01-11 21:32:17 +0530

    regular update

--- init.lua
+++ init.lua
@@ -529,10 +529,10 @@ cmp.setup({
 	},
 	sources = {
 		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
 		{ name = "luasnip" },
 		-- { name = "nvim_lsp_signature_help" },
 		{ name = "path" },
-		{ name = "nvim_lua" },
 		{ name = "buffer" },
 		{ name = "dap" },
 	},
@@ -1110,5 +1110,4 @@ vim.g.copilot_node_command = "~/.nvm/versions/node/v17.9.1/bin/node"
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 vim.g.copilot_tab_fallback = ""
--- vim.keymap.set("i", "<C-F>", vim.fn["copilot#Accept()"], opts)
 vim.api.nvim_set_keymap("i", "<C-F>", "copilot#Accept()", { expr = true })
