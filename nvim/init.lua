684c6082c58586252868143b445469b4abb17991
vanillabase1lb <vanillabase1lb@users.noreply.github.com>
2024-08-06 20:31:01 +0530

    remove deprecated which key functions

--- nvim/init.lua
+++ nvim/init.lua
@@ -203,15 +203,6 @@ require("lazy").setup({
 		event = "VeryLazy", -- Sets the loading event to 'VeryLazy'
 		config = function() -- This is the function that runs, AFTER loading
 			require("which-key").setup()
-
-			-- Document existing key chains
-			require("which-key").register({
-				-- ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-				["<leader>l"] = { name = "[L]SP", _ = "which_key_ignore" },
-				-- ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				-- ['<leader>p'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-			})
 		end,
 	},
 
